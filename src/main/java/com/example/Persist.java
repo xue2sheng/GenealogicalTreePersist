4ddaa581c1e9ab05fe6a15316dc43768c45c6cb6
Andres Sevillano <andres.sevillano.vallejo@gmail.com>
2015-07-20 22:19:57 +0100

    First commit just to get something to document

---
+++ src/main/java/com/example/Persist.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author user
+ */
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class Persist {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Persist.class, args);
+    }
+}
